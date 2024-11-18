38fa540fdcaececa472d6f456af19534b69728f1
jellygas <vologi1@naver.com>
2024-11-18 15:25:26 +0900

    Create Hello2.js

---
+++ Hello2.js
@@ -0,0 +1,18 @@
+function Zerofunction() {
+    let a = document.getElementById('inputA').value;
+    let b = document.getElementById('inputB').value;
+
+    a = Number(a);
+    b = Number(b);
+
+    if(a-b>0) {
+        document.getElementById('result').innerText = "A에서 B를 배면 0보다 큼";
+    }
+    else if(a-b<0){
+        document.getElementById('result').innerText = "A에서 B를 빼면 0보다 작음.";
+    }
+    else{
+        document.getElementById('result').innerText = "A에서 B를 빼면 0임;;";
+
+    }
+}
